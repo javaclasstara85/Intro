cb26d6c6738f745bb7cd63d7a71c0a32e7856ac7
Javaclass <Javaclass@15000529CPU.fec.org>
2017-09-26 15:23:14 -0500

    This is my calculating program i just started

---
+++ src/basics1/Calculator.java
@@ -0,0 +1,16 @@
+package basics1;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+	}
+		// TODO Auto-generated method stub
+	
+		static int calculateSum (int a,int b) {
+			return b;
+		
+					
+		
+	}
+
+}
