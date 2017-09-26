8a8eef1150f0597bf68fa3833cfa5a20c2237a50
Javaclass <Javaclass@15000529CPU.fec.org>
2017-09-26 10:18:27 -0500

    Today I have to pay my phone bill so i can handle my business and be the responsible person I am.

---
+++ src/basics1/MyFirstJavaClass.java
@@ -0,0 +1,10 @@
+package basics1;
+
+public class MyFirstJavaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Good Morning Tara");
+	}
+
+}
