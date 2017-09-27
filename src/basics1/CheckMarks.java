03096070a95579d14d9a2f2944c953d736e71b52
Javaclass <Javaclass@15000529CPU.fec.org>
2017-09-27 14:32:20 -0500

    GradeStudent class

---
+++ src/basics1/CheckMarks.java
@@ -0,0 +1,29 @@
+package basics1;
+public class CheckMarks {
+
+	public static void main(String[] args) {
+	 gradeStudents(72);
+	}
+	//if studentScore is 100 - 90 print grade A
+	static void gradeStudents(int studentScore) {
+	if (studentScore >= 100 && studentScore < 90) {
+	System.out.println("A");
+	}
+	else if (studentScore >= 91 && studentScore < 80){
+		System.out.println("B");
+	}
+		if (studentScore >= 70 && studentScore < 79) {
+		System.out.println("you are not doing good");
+	
+		
+	}
+	}
+	}
+	
+
+	
+	
+	
+				
+
+	
