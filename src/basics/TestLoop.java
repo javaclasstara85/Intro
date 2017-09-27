41f98d27d8841d325d107ad6d48bfda1d7a000ed
Javaclass <Javaclass@15000529CPU.fec.org>
2017-09-27 15:25:45 -0500

    number1

---
+++ src/basics/TestLoop.java
@@ -0,0 +1,40 @@
+package basics;
+
+public class TestLoop  {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub}
+		int number = 1;
+		
+		while(1<=10) { 
+		
+		System.out.println("1");  
+		//System.out.println(holly); {
+		number=number+1;
+			}
+	}
+	}
+		
+			
+
+
+			
+
+	
+	
+	
+		
+			
+				
+				
+			
+				
+			
+	
+
+	
+
+	
+
+
+
