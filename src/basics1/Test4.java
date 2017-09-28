2ba085162f6c782516245ab7bbba6ad828ba0cd9
Javaclass <Javaclass@15000529CPU.fec.org>
2017-09-28 13:08:54 -0500

    40-1

--- src/basics1/Test4.java
+++ src/basics1/Test4.java
@@ -4,12 +4,12 @@ public class Test4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i=40; i>=1; i--) {
+		for(int i=1; i>=40; i++) {
 		System.out.println(i);
 		
 		}
 		
-		for(int i=40; i<=1; i--) {
+		for(int i=1; i<=40; i++) {
 			System.out.println(i);
 		}
 	}
